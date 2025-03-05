c1b1c2bbbdfad07baee8a62e37127cb944488df9
Durval Pereira <duduwhitney@gmail.com>
2025-03-05 11:58:32 -0300

    feat: criada relacao no modelo product com customer

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * Establish a relationship between the product and a customer.
+     *
+     * This function defines a belongs-to relationship where a product belongs
+     * to a single customer. This is useful for retrieving the customer
+     * associated with a given product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function customer(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
